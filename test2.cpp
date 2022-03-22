6f49447885607c8afd11a07efe3229c42148942a
thenamk20 <namlaophu@gmail.com>
2022-03-22 22:29:26 +0700

    add test2.cpp, add function to test1.cpp

--- test2.cpp
+++ test2.cpp
@@ -2,15 +2,10 @@
 
 using namespace std;
 
-int add(int a, int b){
-    return a + b;
-}
-
 int main(){
 
     // don't do anything
-    int b = 100;
-    cout << "hihi" << endl;
+    string s = "test2";
 
     return 0;
 }
